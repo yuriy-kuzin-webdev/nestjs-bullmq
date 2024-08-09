92742624c78555455775e0161603a5858be36522
yuriy-kuzin-webdev <yuriy.kuzin.webdev@gmail.com>
2024-08-09 21:34:30 +0200

    feat: basic bull setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BullModule } from '@nestjs/bull';
 
 @Module({
-  imports: [],
+  imports: [
+    BullModule.forRoot({
+      redis: {
+        host: 'localhost',
+        port: 6379,
+      },
+    }),
+    BullModule.registerQueue({
+      name: 'transcode',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
